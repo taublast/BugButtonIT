051a63a8c2735948c44bd712d92d76baa608ea8d
Nick Kovalsky <nickyboom@mail.ru>
2019-06-25 10:02:08 +0300

    initial

---
+++ BugButtonIT/BugButtonIT/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BugButtonIT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        //-----------------------------------------------------------------------------------------------------------
+        private async void BtnMain_OnClicked(object sender, EventArgs e)
+        //-----------------------------------------------------------------------------------------------------------
+        {
+            var stop = true;
+        }
+
+        //--------------------------------------------------------------------------------------
+        private async void BtnMain_OnReleased(object sender, EventArgs e)
+        //--------------------------------------------------------------------------------------
+        {
+            var stop = true;
+
+        }
+
+        //--------------------------------------------------------------------------------------
+        private async void BtnMain_OnPressed(object sender, EventArgs e)
+        //--------------------------------------------------------------------------------------
+        {
+            var stop = true;
+
+        }
+
+
+
+
+
+    }
+}
